8809c4a4eb9e9ace33bde1fbbfb755b936ed5afe
Sherif Abushadi <sherif@amgando.org>
2015-10-08 15:58:42 -0700

    remove folders "part-1" and "part-2"

--- part-2/app/models/post.rb
+++
@@ -1,7 +0,0 @@
-class Post < ActiveRecord::Base
-  validates_presence_of :body, :title
-
-  def author_name=(name)
-    (!name.kind_of?(String) || name.empty?) ? super("anonymous") : super(name)
-  end
-end
--- part-2/spec/factories/post.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :post do
-    title { "My Thoughts" }
-    author_name { "John Musing" }
-    body { Faker::Lorem.paragraph(5) }
-  end
-end
