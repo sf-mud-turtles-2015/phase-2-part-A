8ee58c07c5e660b83e3e3e473e3d417ee96b1992
suntorytime <stevensm90@gmail.com>
2015-10-09 11:53:53 -0700

    Login, logout, registration with bcrypt operational

--- part-3/app/controllers/index.rb
+++ part-3/app/controllers/index.rb
@@ -6,12 +6,41 @@ get '/' do
 end
 
 ##### LOGIN ####
+post '/login' do
+  user = User.find_by(email: params[:user][:email])
 
+  if user && user.password == params[:user][:password] #
+    session[:email] = user.email # stores the info in cookies
+    redirect '/'
+  else
+    @login_failed = true
+    redirect '/'
+  end
+end
 
 
 ##### REGISTER #####
+get '/register' do
+  erb :register
+end
+
+post '/register' do
+  user = User.new(params[:user])
+  if user.save
+    session[:email] = user.email
+    redirect '/'
+  else
+    # session[:error] = user.errors.messages
+    redirect '/register'
+  end
+end
 
 ##### LOGOUT #####
+
+post '/logout' do
+  session[:email] = nil
+  redirect '/'
+end
 
 ##### CREATE ITEMS #####
 
