236b9cca1a84cbefe33d7bdb83f9a9d942db53a6
Daniel Huang <danielh714@gmail.com>
2015-10-10 13:57:06 -0700

    add helper method and fix bug of editing/deleting/ bidding on closed items

--- part-3/app/helpers/helpers.rb
+++ part-3/app/helpers/helpers.rb
@@ -1,7 +1,25 @@
 helpers do
 
-  def
+  def items_won
+    @won_items = []
+    bids = User.find(session[:id]).bids
 
+    bids.each do |bid|
+      if bid.item.end_date.past?
+        # p 'hello'
+        item = Item.find(bid.item.id)
+        p item
+        max_bid = item.bids.order("bid_amount DESC").first
+
+        if max_bid.user_id == session[:id]
+          @won_items << Item.find(max_bid.item_id)
+        end
+        p "*" * 100
+        p @won_items
+        p "*" * 100
+      end
+    end
+  end
 
 
 
